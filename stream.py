fcd9db2f8166143512f34025eb13e32c89a7218b
jithendar <jithendardharmapuri@gmail.com>
2022-11-28 09:36:41 +0530

    test code commit

--- stream.py
+++ stream.py
@@ -3,5 +3,9 @@ import boto3
 
 def lambda_handler(event, context):
     print('event:::')
-    print(event)
+    print(event, context)
     return event
+
+response = lambda_handler("Jithendar", "Dharmapuri")
+
+print(response)
